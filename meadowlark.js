30e83bd72003c63feaf947632c0a63dc3042e365
Bruno Gazzera <bgazzera@paginar.com>
2015-01-25 14:55:07 -0300

    w

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,24 @@
 var express = require('express');
 var app = express();
+// set up handlebars view engine
+var handlebars = require('express3-handlebars')
+    .create({
+        defaultLayout: 'main'
+    });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res) {
+    res.type('text/plain');
+    res.send('Meadowlark Travel');
+});
+app.get('/about', function(req, res) {
+    res.type('text/plain');
+    res.send('About Meadowlark Travel');
+});
+
 // custom 404 page
 app.use(function(req, res) {
     res.type('text/plain');
